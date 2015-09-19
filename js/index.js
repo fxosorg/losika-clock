6836f496630c0c8523edea2354e2ff456533e6cb
dynamis <dynamis@firehacks.org>
2015-09-19 10:00:35 +0900

    oops, remove unnecessary code

--- js/index.js
+++ js/index.js
@@ -7,17 +7,6 @@ else {
 }
 
 function initialize() {
-  // avoid multiple injection
-  if (document.querySelector('.foxkeh-overlay')) {
-    console.log("Foxkeh Overlay already injected");
-    return; // abort
-  }
+  console.log("initializing add-on...");
   
-  console.log("Injecting Foxkeh Overlay");
-  var div = document.createElement('div');
-  div.classList.add('foxkeh-overlay');
-  document.body.appendChild(div);
-  
-  // cannot use chrome.extension.getURL yet
-  // img.src = chrome.extension.getURL("images/200708.png");
 }
